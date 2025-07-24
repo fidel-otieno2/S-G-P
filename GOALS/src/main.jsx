f12272a9508cbcdba24ddb1c65b9a85e466bdb9d
fidel-otieno2 <fidelmartins05@gmail.com>
2025-07-25 01:26:58 +0300

    mineg

--- GOALS/src/main.jsx
+++ GOALS/src/main.jsx
@@ -2,6 +2,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // or remove if unused
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
